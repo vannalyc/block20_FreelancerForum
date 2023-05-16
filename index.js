8166c96c72b3d272bd7e4f06fd416959ef77c08d
vannalyc <126526667+vannalyc@users.noreply.github.com>
2023-05-15 19:15:41 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const users = [
+    { name: "John", age: 25, occupation: "gardener" },
+    { name: "Lenny", age: 51, occupation: "programmer" },
+    { name: "Andrew", age: 43, occupation: "teacher" },
+    { name: "Peter", age: 81, occupation: "teacher" },
+    { name: "Anna", age: 43, occupation: "teacher" },
+    { name: "Albert", age: 76, occupation: "programmer" },
+    { name: "Adam", age: 47, occupation: "teacher" },
+    { name: "Robert", age: 72, occupation: "driver" },
+];
+​
+function main() {
+​
+}
+​
+//call the main function
+main();
